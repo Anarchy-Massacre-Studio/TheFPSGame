f6525077e52684c6e84283de81cac9c4db19af2b
kevin56348 <30772570+kevin56348@users.noreply.github.com>
2018-02-23 22:54:40 +0800

    QUIZ_NO001
    QUIZ_NO001

---
+++ Assets/Shader/PostProcessing/ScreenFade.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFade : PostEffectBase 
+{
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		Graphics.Blit (src, dest, _Material);
+	}
+}
